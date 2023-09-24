75dadf00aa8cb820725e82c14b415ebd9f10f3c6
meghamandekar22 <130079079+meghamandekar22@users.noreply.github.com>
2023-09-24 22:02:18 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,27 @@
+let movies = [
+    {
+        name: 'falcon and the winter soldier',
+        des: 'Falcon and the Winter Soldier" is a Marvel series. It follows Sam Wilson (Falcon) and Bucky Barnes (Winter Soldier) as they navigate post-"Avengers: Endgame" challenges. Sam considers taking up Captain America mantle, while Bucky deals with his past. The show blends action, character growth, and social commentary within the Marvel universe.',
+        image: 'images/slider 2.PNG'
+    },
+    {
+        name: 'loki',
+        des: 'Loki is a Marvel series. It stars the character Loki, the god of mischief, following his escape with the Tesseract in "Avengers: Endgame." The show explores a new timeline and variant of Loki, who faces the Time Variance Authority (TVA) and confronts his own past and identity. Its a blend of time-travel, character development, and intrigue in the Marvel universe.',
+        image: 'images/slider 1.PNG'
+    },
+    {
+        name: 'wanda vision',
+        des: 'WandaVision is a Marvel series. It features Wanda Maximoff (Scarlet Witch) and Vision in a sitcom-inspired reality-warping setting. The show explores grief, identity, and the consequences of Wanda immense powers. It offers a unique blend of comedy, drama, and mystery within the Marvel Cinematic Universe.',
+        image: 'images/slider 3.PNG'
+    },
+    {
+        name: 'raya and the last dragon',
+        des: 'The story is set in the fantasy world of Kumandra, where a young warrior named Raya embarks on a quest to find the last dragon, Sisu, in order to save her fractured land from an ancient evil called the Druun. The film explores themes of trust, unity, and the power of friendship. Its known for its stunning animation, strong female lead, and cultural influences drawn from various Southeast Asian traditions, making it a visually captivating and culturally resonant Disney film.',
+        image: 'images/slider 4.PNG'
+    },
+    {
+        name: 'luca',
+        des: 'The story is set in a picturesque coastal town on the Italian Riviera and follows a young sea monster named Luca who transforms into a human when on land. He forms a deep friendship with another sea monster, Alberto, and together they explore the human world while keeping their true identities a secret. The film beautifully portrays themes of friendship, acceptance, and the joy of discovery. "Luca" is known for its charming characters, stunning visuals, and heartwarming storytelling, making it a delightful addition to the Pixar filmography.',
+        image: 'images/slider 5.PNG'
+    }
+]
